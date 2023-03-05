71447ac83ecf269953d829f9b97668737bff6f06
Janis <janis.meister87@gmail.com>
2023-03-05 17:03:12 +0100

    add field functinallity

--- Assets/Scripts/Field/FieldController.cs
+++ Assets/Scripts/Field/FieldController.cs
@@ -3,12 +3,35 @@ using UnityEngine;
 
 public class FieldController : Interactable
 {
-  public Field field;
-  public Crop testCrop;
+  private Field field;
+
+  public override string interactText => field.State == FieldState.EMPTY ? "Plant" : field.State == FieldState.HARVESTABLE ? "Harvest" : field.State == FieldState.GROWING && !field.IsWatered ? "Water" : "Empty";
+
+  public void SetField(Field field)
+  {
+    this.field = field;
+  }
 
   public override void OnInteract()
   {
     Debug.Log("Interacting with field");
-    field.Plant(testCrop);
+    if (field.State == FieldState.EMPTY)
+    {
+      if (FarmManager.Instance.CropInventory.Items.Length > 0)
+        field.Plant(FarmManager.Instance.CropInventory.Items[0].item);
+    }
+    else if (field.State == FieldState.HARVESTABLE)
+    {
+      field.Harvest();
+    }
+    else if (field.State == FieldState.GROWING && !field.IsWatered)
+    {
+      field.Water();
+    }
+    else
+    {
+      field.EmptyField();
+    }
+
   }
 }
